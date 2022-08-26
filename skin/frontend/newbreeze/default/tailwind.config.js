ba0c947cf48b16ead8b03cf93542f247d4459471
Fabrizio Balliano <fabrizio.balliano@gmail.com>
2022-08-26 17:48:53 +0100

    Initial release

---
+++ skin/frontend/newbreeze/default/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./css/styles.src.css"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
---
+++ skin/frontend/newbreeze/default/watch.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+npx tailwindcss -i ./css/styles.src.css -o ./css/styles.css --watch --minify
